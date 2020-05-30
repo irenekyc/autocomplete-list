7c1bff979ce053e9454372bea540608e9296997c
irenekyc <gugu_irene@hotmail.com>
2020-05-30 11:50:32 +0800

    finished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Fragment, useState} from 'react';
+import Spinner from './Spinner'
+import {debounce} from 'lodash'
+import axios from 'axios'
 import './App.css';
 
 function App() {
+  const [countries, setCountries] = useState(null)
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(false)
+  const changeHandler = debounce((query)=>{
+    setError(false)
+    setLoading(true)
+    fetchData(query)
+  }, 500)
+
+  const fetchData = async (query)=>{
+    let response
+    try{
+      if(query.length>=1){
+        response = await axios.get(`https://restcountries.eu/rest/v2/name/${query}`)
+        setCountries(response.data)
+      } else if (query === ""){
+          setCountries(null)}
+    }
+    catch(err){
+      setError(true)
+    }
+    setLoading(false)      
+    }
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <Fragment>
+    <h1> Autocomplete List</h1>
+    <div className="inputContainer">
+      <input type="text" onChange = {(e)=>changeHandler(e.target.value)} className="inputField"/>
+      {loading && (<div className="loadingContainer">
+        <Spinner />
+        </div>)}
+      {countries!==null && countries.length>=1 && (
+        <div className="listContainer">
+      {countries.map(e=>{
+       return <li key={e.name}> {e.name}</li>
+      })}
+    </div>)}
+    {error && (
+      <div className="listContainer">
+        <li> No result found.</li>
+      </div>
+    )}
     </div>
+  </Fragment>
   );
 }
 
