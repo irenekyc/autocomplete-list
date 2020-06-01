51145672308db7c75f637af3a5dde21db6d61610
irenekyc <gugu_irene@hotmail.com>
2020-06-01 15:21:04 +0800

    Updates

---
+++ precache-manifest.d4e8a1c01b0c060a95d6cc7ff719c74a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e012e7b3149a0d631067ee5ac95df0c",
+    "url": "/autocomplete-list/index.html"
+  },
+  {
+    "revision": "2f8e10faa41cc7507d2e",
+    "url": "/autocomplete-list/static/css/main.eb334cde.chunk.css"
+  },
+  {
+    "revision": "de525f6a839499566e35",
+    "url": "/autocomplete-list/static/js/2.04e07e89.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/autocomplete-list/static/js/2.04e07e89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f8e10faa41cc7507d2e",
+    "url": "/autocomplete-list/static/js/main.77c3d7a7.chunk.js"
+  },
+  {
+    "revision": "80608a9d0a83bce7b3df",
+    "url": "/autocomplete-list/static/js/runtime-main.288815a4.js"
+  }
+]);
